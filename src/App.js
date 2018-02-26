36c4723996e286b8cbddeac173b6906e0582b16d
Beck TJ <bakytbek.tatibekov@gmail.com>
2018-02-25 19:10:58 -0600

    conflicts

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          test with Michael!! test with Michael!! test with Michael!!
+          test with Michael!! test with Michael!! test with Michael!!<br />djvbhhjbd
         </p>
       </div>
     );
